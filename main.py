1ad1e8a668beb17b354d3846464cdcc4adf6e793
Yoni Yang <76271912+yoni13@users.noreply.github.com>
2024-10-11 16:09:46 +0800

    Update main.py

--- main.py
+++ main.py
@@ -9,9 +9,9 @@ def download_and_build_rknn(repo_name,repo_url):
     subprocess.run(["git", "clone", repo_url])
     os.chdir(repo_name)
     if os.path.exists("textual"):
-        subprocess.run(["python3", "build_rknn.py", "--model", "textual/model.onnx"])
+        subprocess.run(["python3", "../../build_rknn.py", "--model", "textual/model.onnx"])
     if os.path.exists("visual"):
-        subprocess.run(["python3", "build_rknn.py", "--model", "visual/model.onnx"])
+        subprocess.run(["python3", "../../build_rknn.py", "--model", "visual/model.onnx"])
     subprocess.run(["ls"])
     os.chdir("..")
     shutil.rmtree(repo_name)
