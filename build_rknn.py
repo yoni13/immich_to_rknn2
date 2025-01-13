134e769704d19ce1891b3aafdb7ed9d4cf17d903
Yoni Yang <76271912+yoni13@users.noreply.github.com>
2025-01-13 13:25:36 +0800

    Clean up.

--- build_rknn.py
+++ build_rknn.py
@@ -1,7 +1,8 @@
 import argparse
 import os
+
 parser = argparse.ArgumentParser("RKNN model converting")
-parser.add_argument("model", help="Directory of the model that will be exported to rknn format ex:ViT-B-32__openai.", type=str)
+parser.add_argument("model", help="Directory of the model that will be exported to RKNN ex:ViT-B-32__openai.", type=str)
 parser.add_argument("target_platform", help="target platform ex:rk3566", type=str)
 args = parser.parse_args()
 
@@ -34,7 +35,7 @@ def ConvertModel(model_path='ViT-B-32__openai/textual/model.onnx', target_platfo
 
     if not os.path.isfile(f'{model_path.replace("onnx","rknn")}'):
         print(f'Dummy model not found at {model_path.replace("onnx","rknn")}, creating one')
-        with open(f'{model_path.replace("onnx","rknn")}', 'w') as f:
+        with open(f'{model_path.replace("onnx","rknn")}', 'w'):
             pass
 
 
@@ -45,9 +46,8 @@ if os.path.isdir(f'{args.model}/textual') and os.path.isdir(f'{args.model}/visua
 
 elif os.path.isdir(f'{args.model}/detection') and os.path.isdir(f'{args.model}/recognition'): # is a facial model
     print('Converting facial model.')
-    ConvertModel(model_path=f'{args.model}/detection/model.onnx', target_platform=args.target_platform, dynamic_input=[[[1, 3, 640, 640]]])
-    # I just used some common faces, if immich only uses one, keep one should be fine
-    ConvertModel(model_path=f'{args.model}/recognition/model.onnx', target_platform=args.target_platform, dynamic_input=[[[1, 3, 112, 112]], [[4, 3, 112, 112]], [[8, 3, 112, 112]]])
+    ConvertModel(f'{args.model}/detection/model.onnx', [[[1, 3, 640, 640]]])
+    ConvertModel(f'{args.model}/recognition/model.onnx', args.target_platform, [[[1, 3, 112, 112]]])
 
 else:
     print('Unknown model.')
