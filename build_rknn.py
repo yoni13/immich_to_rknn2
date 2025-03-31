5eeca8067368e041f843042996d4064eb5dd01fa
Yoni Yang <76271912+yoni13@users.noreply.github.com>
2025-03-31 09:46:52 +0800

    Update build_rknn.py

--- build_rknn.py
+++ build_rknn.py
@@ -51,11 +51,11 @@ def ConvertModel(model_path='ViT-B-32__openai/textual/model.onnx', target_platfo
     if ret != 0:
         print("Build failed!")
         exit(ret)
-
     print(model_path.replace('model.onnx',f'{target_platform}.rknn'))
-    ret = rknn.init_runtime(target=target_platform, perf_debug=True)
-    perf_detail = rknn.eval_perf()
-    print(perf_detail)
+    ret = rknn.accuracy_analysis(inputs=["rand.npy"], target=target_platform)
+    if ret != 0:
+        print("Accuracy analysis failed!")
+        exit(ret)
     ret = rknn.export_rknn(model_path.replace('model.onnx',f'{target_platform}.rknn'))
     print(f"RKNN export reported with status {ret}")
     if ret != 0:
