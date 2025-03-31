c80d6572f3cabe5689c1ed06cb80781ad85b6f0d
Yoni Yang <76271912+yoni13@users.noreply.github.com>
2025-03-31 10:01:19 +0800

    Update convert.sh

--- convert.sh
+++ convert.sh
@@ -8,6 +8,8 @@ if [ $python_exit_status -ne 0 ]; then
     echo -e "\e[31mError: Python script failed with exit code $python_exit_status.\e[0m"
     echo "Check immich_to_rknn2.log for Python errors or details."
     cat immich_to_rknn2.log
+    cat ./snapshot/error_analysis.txt
+    cat ./snapshot/map_name_to_file.txt
     exit $python_exit_status
 fi
 
@@ -15,6 +17,8 @@ fi
 if grep -q "No lowering found for" immich_to_rknn2.log; then
     echo -e "\e[31mSome operations are not supported by RKNN, please check the log file for details.\e[0m"
     cat immich_to_rknn2.log
+    cat ./snapshot/error_analysis.txt
+    cat ./snapshot/map_name_to_file.txt
     exit 1
 else
     echo -e "\e[32mConversion completed successfully.\e[0m"
